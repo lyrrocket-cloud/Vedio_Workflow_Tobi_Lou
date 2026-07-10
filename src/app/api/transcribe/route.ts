aa510c4519daa9bc32e5ea8bb811343df8315014
user2666011526 <3669488669299752-user2666011526@noreply.coze.cn>
2026-07-11 00:06:11 +0800

    fix: FCPXML 字幕显示默认"标题"问题 - 改用 text 元素替代 param
    移除 Basic Title 模板引用和 param 元素（key 不正确导致文本未设置），
    改用 <text> 元素直接嵌入字幕内容，不再依赖外部模板。
    
    Coze-Commit-Type: user
    Coze-User-ID: 3669488669299752
    Coze-Conversation-ID: 2521754

--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -92,8 +92,8 @@ export function generateFCPXML(segments: SubtitleSegment[], frameRate: number = 
       .replace(/"/g, '&quot;')
       .replace(/'/g, '&apos;');
     return `
-            <title name="字幕 ${seg.id}" lane="1" offset="${start}" ref="r2" duration="${duration}">
-                <param name="Text" key="9999/999166631/999166633/1/100" value="${escapedText}"/>
+            <title name="字幕 ${seg.id}" lane="1" offset="${start}" duration="${duration}">
+                <text>${escapedText}</text>
             </title>`;
   }).join('');
 
@@ -102,7 +102,6 @@ export function generateFCPXML(segments: SubtitleSegment[], frameRate: number = 
 <fcpxml version="1.10">
     <resources>
         <format id="r1" name="${formatName}" frameDuration="${frameDuration}" width="1920" height="1080"/>
-        <effect id="r2" name="Basic Title" uid=".../Titles.localized/Bumper:Opener.localized/Basic Title.localized/Basic Title.moti"/>
     </resources>
     <library>
         <event name="字幕项目">
